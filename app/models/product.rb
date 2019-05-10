63b92af0d50e37f698bb2d15b6f1d65a45b7d8f4
i__kobe <issei.o_o.iverson@icloud.com>
2019-05-10 16:27:29 +0900

    Edit row

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
+  has_many :images, foreign_key: "item_id", dependent: :delete_all
   has_many :item_categories, foreign_key: "item_id"
   belongs_to :user, optional: true, foreign_key: "seller_id"
   accepts_nested_attributes_for :images, allow_destroy: true, :reject_if => :all_blank
-  has_many :images, foreign_key: "item_id", dependent: :delete_all
   has_many :statuses, foreign_key: "item_id", dependent: :delete_all
   has_many :item_categories, foreign_key: "item_id"
   scope :get_random_product, -> (id){ where("parent_category_id = ?", id).order("RAND()").limit(4) }
